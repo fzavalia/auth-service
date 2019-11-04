816fc28d8b0279541f9533d77794764109957240
Fernando Zavalia <fzavalia@mulesoft.com>
2019-11-04 14:06:42 -0300

    Login without inteface

--- src/actions/Login/Login.ts
+++ src/actions/Login/Login.ts
@@ -1,25 +1,66 @@
-import AccountRepository, {
-  Account
-} from "../../repository/AccountRepository/AccountRepository";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import AccountRepository from "../../repository/AccountRepository/AccountRepository";
 import AccessTokenRepository from "../../repository/AccessTokenRepository/AccessTokenRepository";
 import RefreshTokenRepository from "../../repository/RefreshTokenRepository/RefreshTokenRepository";
 
-export interface LoginResult {
-  accessToken: string;
-  refreshToken: string;
+class Login {
+  constructor(
+    private readonly accountRepository: AccountRepository,
+    private readonly tokenFactory: TokenFactory
+  ) {}
+
+  exec = async (username: string, password: string) => {
+    const account = await this.accountRepository.find(username);
+    await this.validatePassword(password, account.password);
+    return await this.tokenFactory.makeTokens(account.username);
+  };
+
+  private validatePassword = async (
+    inputPassword: string,
+    accountPassword: string
+  ) => {
+    const match = await bcrypt.compare(inputPassword, accountPassword);
+    if (!match) {
+      throw new Error();
+    }
+  };
 }
 
-interface Login {
-  exec: (username: string, password: string) => Promise<LoginResult>;
-}
+class TokenFactory {
+  constructor(
+    private readonly atRepository: AccessTokenRepository,
+    private readonly rtRepository: RefreshTokenRepository
+  ) {}
 
-export interface Salt {
-  validate: (data: string, hash: string) => Promise<void>;
-}
+  makeTokens = async (accountUsername: string) => {
+    const at = await this.makeAccessToken(accountUsername);
+    const rt = await this.makeRefreshToken(accountUsername, at);
+    return { accessToken: at, refreshToken: rt };
+  };
 
-export interface TokenFactory {
-  makeAccessToken: () => Promise<string>;
-  makeRefreshToken: () => Promise<string>;
+  makeAccessToken = async (accountUsername: string) => {
+    await this.atRepository.invalidateAllForAccount(accountUsername);
+    const at = jwt.sign("foo", "secret");
+    await this.atRepository.create({
+      value: at,
+      valid: true,
+      accountUsername: accountUsername
+    });
+    return at;
+  };
+
+  makeRefreshToken = async (accountUsername: string, accessToken: string) => {
+    await this.rtRepository.invalidateAllForAccount(accountUsername);
+    const rt = jwt.sign("foo", "secret");
+    await this.rtRepository.create({
+      value: rt,
+      valid: true,
+      accountUsername: accountUsername,
+      accessTokenValue: accessToken
+    });
+    return rt;
+  };
 }
 
 export default Login;
