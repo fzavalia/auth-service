2ca7d91397c04322292c0b51070a54740be6ba33
Fernando Zavalia <fzavalia@mulesoft.com>
2019-11-04 12:25:04 -0300

    Uncomment inmemory implementation

--- src/repository/AccountRepository.ts
+++ src/repository/AccountRepository.ts
@@ -10,42 +10,42 @@ interface AccountRepository {
   update: (username: string, account: Account) => Promise<void>;
 }
 
-// export class InMemoryAccountRepository implements AccountRepository {
-//   constructor(private readonly accounts: Map<string, Account>) {}
+export class InMemoryAccountRepository implements AccountRepository {
+  constructor(private readonly accounts: Map<string, Account>) {}
 
-//   create = (account: Account) =>
-//     new Promise<void>((resolve, reject) => {
-//       if (this.accounts.has(account.username)) {
-//         reject(new AccountAlreadyExists());
-//       } else {
-//         this.accounts.set(account.username, account);
-//         resolve();
-//       }
-//     });
+  create = (account: Account) =>
+    new Promise<void>((resolve, reject) => {
+      if (this.accounts.has(account.username)) {
+        reject(new AccountAlreadyExists());
+      } else {
+        this.accounts.set(account.username, account);
+        resolve();
+      }
+    });
 
-//   find = (username: string) =>
-//     new Promise<Account>((resolve, reject) => {
-//       const account = this.accounts.get(username);
-//       if (!account) {
-//         reject(new AccountNotFound());
-//       } else {
-//         resolve(account);
-//       }
-//     });
+  find = (username: string) =>
+    new Promise<Account>((resolve, reject) => {
+      const account = this.accounts.get(username);
+      if (!account) {
+        reject(new AccountNotFound());
+      } else {
+        resolve(account);
+      }
+    });
 
-//   update = (username: string, account: Account) =>
-//     new Promise<void>((resolve, reject) => {
-//       if (!this.accounts.has(username)) {
-//         reject(new AccountNotFound());
-//       } else {
-//         this.accounts.set(username, account);
-//         resolve();
-//       }
-//     });
-// }
+  update = (username: string, account: Account) =>
+    new Promise<void>((resolve, reject) => {
+      if (!this.accounts.has(username)) {
+        reject(new AccountNotFound());
+      } else {
+        this.accounts.set(username, account);
+        resolve();
+      }
+    });
+}
 
-// export class AccountAlreadyExists extends Error {}
+export class AccountAlreadyExists extends Error {}
 
-// export class AccountNotFound extends Error {}
+export class AccountNotFound extends Error {}
 
 export default AccountRepository;
