bb5202192cca365c4d44d465842f6034edd0c2ea
Fernando Zavalia <fzavalia@mulesoft.com>
2019-11-06 19:59:59 -0300

    Fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-const parseIntFromEnv = (env: string, def: number) => (process.env[env] ? parseInt(process.env[env]) : def);
+const parseIntFromEnv = (env: string | undefined, def: number) => (env ? parseInt(env) : def);
 
-export const accessTokenExpiration = parseIntFromEnv("ACCESS_TOKEN_EXPIRATION", 30);
-export const refreshTokenExpiration = parseIntFromEnv("REFRESH_TOKEN_EXPIRATION", 10080);
-export const activationSecretExpiration = parseIntFromEnv("ACTIVATION_SECRET_EXPIRATION", 1440);
+export const accessTokenExpiration = parseIntFromEnv(process.env.ACCESS_TOKEN_EXPIRATION, 30);
+export const refreshTokenExpiration = parseIntFromEnv(process.env.REFRESH_TOKEN_EXPIRATION, 10080);
+export const activationSecretExpiration = parseIntFromEnv(process.env.ACTIVATION_SECRET_EXPIRATION, 1440);
