d38cb57889ee78acc16937e473440609ff431cb8
Fernando Zavalia <fzavalia@mulesoft.com>
2019-11-08 10:31:56 -0300

    Refactor

--- src/repository/typeOrm/TypeOrmRepository.ts
+++ src/repository/typeOrm/TypeOrmRepository.ts
@@ -1,21 +1,27 @@
-import { createConnection } from "typeorm";
+import { createConnection, ConnectionOptions } from "typeorm";
 import "reflect-metadata";
 
+const connectionConfig: ConnectionOptions = {
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "postgres",
+  entities: [__dirname + "/entities/*.js"],
+  synchronize: true,
+  logging: false,
+};
+
 abstract class TypeOrmRepository {
-  static createConnection = () =>
-    createConnection({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "postgres",
-      entities: [__dirname + "/entities/*.js"],
-      synchronize: true,
-      logging: false,
-    })
-      .then(() => console.log("Connected to Database"))
-      .catch(() => console.log("Could not connect to Database"));
+  static createConnection = async () => {
+    try {
+      await createConnection(connectionConfig);
+      console.log("Connected to Database");
+    } catch (e) {
+      console.log("Could not connect to Database");
+    }
+  };
 }
 
 export default TypeOrmRepository;
