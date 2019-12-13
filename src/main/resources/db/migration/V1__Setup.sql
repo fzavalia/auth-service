131243cf1e85aa6a82a226aea6251b3e9358390f
Fernando Zavalia <zavaliafernando@gmail.com>
2019-12-13 02:01:43 -0300

    Store AccessTokens and Validate them
    Store AccessTokens and Validate them

--- src/main/resources/db/migration/V1__Setup.sql
+++ src/main/resources/db/migration/V1__Setup.sql
@@ -1,4 +1,11 @@
 CREATE TABLE accounts (
     username TEXT PRIMARY KEY,
     password TEXT NOT NULL
-)
+);
+
+CREATE TABLE access_tokens (
+    value TEXT PRIMARY KEY,
+    username TEXT NOT NULL REFERENCES accounts(username),
+    created TIMESTAMP NOT NULL,
+    deprecated BOOLEAN NOT NULL
+);
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import java.util.Date
+import java.sql.Timestamp
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
@@ -7,15 +7,13 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{HttpResponse, StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import org.mindrot.jbcrypt.BCrypt
+import slick.jdbc.PostgresProfile.api._
 import spray.json._
+
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
 import scala.io.StdIn
-import scala.util.Try
-import scala.concurrent.duration._
-import slick.jdbc.PostgresProfile.api._
 
 case class RegisterRequest(username: String, password: String, passwordConfirmation: String)
 case class LoginRequest(username: String, password: String)
@@ -48,12 +46,12 @@ object Main extends App with DefaultJsonProtocol with SprayJsonSupport {
       if (Password.isInvalid(req.password, account.password)) {
         completeWithError(StatusCodes.Unauthorized, "Invalid Credentials!")
       } else {
-        val accessToken: String = AccessTokenFactory.make
+        val accessToken = AccessTokenRepository.create(req.username)
         complete(LoginResponse(accessToken))
       }
     },
     (path("auth") & post & entity(as[AuthRequest])) { req =>
-      if (AccessTokenValidator.isInvalid(req.accessToken)) {
+      if (!AccessTokenRepository.isValid(req.accessToken)) {
         completeWithError(StatusCodes.Unauthorized, "Invalid Access Token!")
       } else {
         complete(HttpResponse(StatusCodes.OK))
@@ -69,7 +67,7 @@ object Main extends App with DefaultJsonProtocol with SprayJsonSupport {
   private val bindingFuture: Future[Http.ServerBinding] =
     Http().bindAndHandle(routes, "0.0.0.0", 8080)
 
-  println("Listening on http://0.0.0.0:8008")
+  println("Listening on http://0.0.0.0:8080")
 
   StdIn.readLine()
 
@@ -78,16 +76,19 @@ object Main extends App with DefaultJsonProtocol with SprayJsonSupport {
     .onComplete(_ => system.terminate())
 }
 
+object DBConnection {
+  val db = Database.forConfig("postgres")
+}
+
 object AccountRepository {
 
-  val db = Database.forConfig("postgres")
+  import DBConnection._
 
   case class AccountsTableRow(username: String, password: String)
 
   class AccountsTable(tag: Tag) extends Table[AccountsTableRow](tag, "accounts") {
     def username = column[String]("username", O.PrimaryKey)
     def password = column[String]("password")
-    def foo      = column[String]("foo")
     def *        = (username, password) <> (AccountsTableRow.tupled, AccountsTableRow.unapply)
   }
 
@@ -107,34 +108,52 @@ object AccountRepository {
 
 case class Account(username: String, password: String)
 
+object AccessTokenRepository {
+
+  import DBConnection._
+
+  case class AccessTokenTableRow(value: String, username: String, created: Timestamp, deprecated: Boolean)
+
+  class AccessTokenTable(tag: Tag) extends Table[AccessTokenTableRow](tag, "access_tokens") {
+    def value      = column[String]("value", O.PrimaryKey)
+    def username   = column[String]("username")
+    def created    = column[Timestamp]("created")
+    def deprecated = column[Boolean]("deprecated")
+    def *          = (value, username, created, deprecated) <> (AccessTokenTableRow.tupled, AccessTokenTableRow.unapply)
+    def account    = foreignKey("account_fk", username, AccountRepository.accounts)(_.username)
+  }
+
+  val accessTokens = TableQuery[AccessTokenTable]
+
+  def create(username: String): String = {
+    val deprecate = accessTokens
+      .filter(_.username === username)
+      .map(_.deprecated)
+      .update(true)
+    val accessToken         = java.util.UUID.randomUUID().toString
+    val nowMillis           = System.currentTimeMillis()
+    val created             = new Timestamp(nowMillis)
+    val accessTokenTableRow = AccessTokenTableRow(accessToken, username, created, deprecated = false)
+    val create              = accessTokens += accessTokenTableRow
+    Await.result(db.run(DBIO.seq(deprecate, create).transactionally), 5.seconds)
+    accessToken
+  }
+
+  def isValid(accessToken: String): Boolean = {
+    val listAccessTokens = accessTokens
+      .filter(_.value === accessToken)
+      .filter(_.deprecated === false)
+      .result
+    val queryResult = Await.result(db.run(listAccessTokens), 5.seconds)
+    val limit       = new Timestamp(System.currentTimeMillis - TimeUnit.DAYS.toMillis(1))
+    queryResult.nonEmpty && queryResult.forall(_.created.after(limit))
+  }
+}
+
 object Password {
   private val rounds                                    = 10
   private val salt: String                              = BCrypt.gensalt(rounds)
   def hash(password: String): String                    = BCrypt.hashpw(password, salt)
   def isValid(plain: String, hashed: String): Boolean   = BCrypt.checkpw(plain, hashed)
   def isInvalid(plain: String, hashed: String): Boolean = !isValid(plain, hashed)
-}
-
-object AccessTokenFactory {
-
-  private val secret = "secret"
-
-  val algorithm: Algorithm = Algorithm.HMAC256(secret)
-
-  def make: String = {
-    val nowMillis        = System.currentTimeMillis()
-    val expirationMillis = nowMillis + TimeUnit.DAYS.toMillis(1)
-    val issuedAt         = new Date(nowMillis)
-    val expiresAt        = new Date(expirationMillis)
-    JWT
-      .create()
-      .withIssuedAt(issuedAt)
-      .withExpiresAt(expiresAt)
-      .sign(algorithm)
-  }
-}
-
-object AccessTokenValidator {
-  def isInvalid(accessToken: String): Boolean =
-    Try(JWT.require(AccessTokenFactory.algorithm).build().verify(accessToken)).isFailure
 }
